95bcbc73ab3b77d797dbbb0d1e3523ca8894c121
Ubuntu <ubuntu@ip-172-31-89-211.ec2.internal>
2021-11-28 21:43:57 +0000

    creating vpc security group

---
+++ networking/security_group.tf
@@ -0,0 +1,45 @@
+#
+
+resource "aws_security_group" "master_sg" {
+    name = "vpc_web_sg"
+    description = "Allow incoming HTTP connections."
+     vpc_id = aws_vpc.main.id
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+   
+    
+    tags = {
+    Name = "master-SG"
+    Environment = var.environment
+    }
+}
